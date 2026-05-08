2a13ac724cc54498f7850673e5f5b48ef92ba73f
Minhal Fatima <minhalfatima@gmail.com>
2026-05-08 15:57:52 +0500

    Add descriptive statistics on feature branch

--- data.py
+++ data.py
@@ -8,3 +8,27 @@ df = pd.read_csv("data/StudentsPerformance.csv")
 print(df.head())
 
 print(df.info())
+
+# Descriptive Statistics
+print("\n--- Descriptive Statistics ---")
+print(df.describe())
+print("\n--- Mean Scores ---")
+print(df[['math score', 'reading score', 'writing score']].mean())
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("data/StudentsPerformance.csv")
+
+print(df.head())
+print(df.info())
+
+# Descriptive Statistics
+print(df.describe())
+print(df[['math score', 'reading score', 'writing score']].mean())
+
+
+
